1b3ebaa72b4a3db21fe1cb9667e8a778c5ad0ef6
Pete Moore <pmoore@mozilla.com>
2016-09-29 19:50:01 +0200

    Regenerated library to pick up latest schema changes

---
+++ src/main/java/org/mozilla/taskcluster/client/pulse/Pulse.java
@@ -0,0 +1,72 @@
+// The following code is AUTO-GENERATED. Please DO NOT edit.
+//
+// This package was generated from the schema defined at
+// http://references.taskcluster.net/pulse/v1/api.json
+package org.mozilla.taskcluster.client.pulse;
+
+import org.mozilla.taskcluster.client.APICallFailure;
+import org.mozilla.taskcluster.client.CallSummary;
+import org.mozilla.taskcluster.client.Credentials;
+import org.mozilla.taskcluster.client.EmptyPayload;
+import org.mozilla.taskcluster.client.TaskClusterRequestHandler;
+
+/**
+ * The taskcluster-pulse service, typically available at `pulse.taskcluster.net`
+ * manages pulse credentials for taskcluster users.
+ * 
+ * A service to manage Pulse credentials for anything using
+ * Taskcluster credentials. This allows us self-service and
+ * greater control within the Taskcluster project.
+ *
+ * @see "[Pulse API Documentation](https://docs.do.not.exist.yet.service.not.in.production)"
+ */
+public class Pulse extends TaskClusterRequestHandler {
+
+    protected static final String defaultBaseURL = "https://pulse.taskcluster.net/v1";
+
+    public Pulse(Credentials credentials) {
+        super(credentials, defaultBaseURL);
+    }
+
+    public Pulse(Credentials credentials, String baseURL) {
+        super(credentials, baseURL);
+    }
+
+    public Pulse(String clientId, String accessToken) {
+        super(new Credentials(clientId, accessToken), defaultBaseURL);
+    }
+
+    public Pulse(String clientId, String accessToken, String certificate) {
+        super(new Credentials(clientId, accessToken, certificate), defaultBaseURL);
+    }
+
+    public Pulse(String baseURL) {
+        super(baseURL);
+    }
+
+    public Pulse() {
+        super(defaultBaseURL);
+    }
+
+    /**
+     * Documented later...
+     * 
+     * **Warning** this api end-point is **not stable**.
+     *
+     * @see "[Ping Server API Documentation](https://docs.do.not.exist.yet.service.not.in.production#ping)"
+     */
+    public CallSummary<EmptyPayload, EmptyPayload> ping() throws APICallFailure {
+        return apiCall(null, "GET", "/ping", EmptyPayload.class);
+    }
+
+    /**
+     * An overview of the Rabbit cluster
+     * 
+     * **Warning** this api end-point is **not stable**.
+     *
+     * @see "[Rabbit Overview API Documentation](https://docs.do.not.exist.yet.service.not.in.production#overview)"
+     */
+    public CallSummary<EmptyPayload, RabbitOverviewResponse> overview() throws APICallFailure {
+        return apiCall(null, "GET", "/overview", RabbitOverviewResponse.class);
+    }
+}
