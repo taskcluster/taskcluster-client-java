2027c605ba0b66a7fb5894512e3daec542742252
Pete Moore <pmoore@mozilla.com>
2016-10-14 16:34:08 +0200

    Regenerated library to pick up latest schema changes

--- src/main/java/org/mozilla/taskcluster/client/auth/Auth.java
+++ src/main/java/org/mozilla/taskcluster/client/auth/Auth.java
@@ -460,9 +460,8 @@ public class Auth extends TaskClusterRequestHandler {
     }
 
     /**
-     * Documented later...
-     * 
-     * **Warning** this api end-point is **not stable**.
+     * Respond without doing anything.
+     * This endpoint is used to check that the service is up.
      *
      * @see "[Ping Server API Documentation](https://docs.taskcluster.net/reference/platform/auth/api-docs#ping)"
      */
