da26e3767fcee83d00c0757382421a1a40ef94dc
Pete Moore <pmoore@mozilla.com>
2016-11-28 22:29:40 +0100

    Regenerated library to pick up latest schema changes

--- src/main/java/org/mozilla/taskcluster/client/login/Login.java
+++ src/main/java/org/mozilla/taskcluster/client/login/Login.java
@@ -16,19 +16,6 @@ import org.mozilla.taskcluster.client.TaskClusterRequestHandler;
  * https://tools.taskcluster.net.  If you are working on federating logins
  * with TaskCluster, this is probably *not* the service you are looking for.
  * Instead, use the federated login support in the tools site.
- * 
- * The API methods described here issue temporary credentials based on
- * an assertion.  The assertion identifies the user, usually with an
- * email-like string.  This string is then passed through a series of
- * authorizers, each of which may supply scopes to be included in the
- * credentials. Finally, the service generates temporary credentials based
- * on those scopes.
- * 
- * The generated credentials include scopes to create new, permanent clients
- * with names based on the user's identifier.  These credentials are
- * periodically scanned for scopes that the user does not posess, and disabled
- * if such scopes are discovered.  Thus users can create long-lived credentials
- * that are only usable until the user's access level is reduced.
  *
  * @see "[Login API Documentation](https://docs.taskcluster.net/reference/core/login/api-docs)"
  */
@@ -58,19 +45,6 @@ public class Login extends TaskClusterRequestHandler {
 
     public Login() {
         super(defaultBaseURL);
-    }
-
-    /**
-     * Given an [assertion](https://developer.mozilla.org/en-US/Persona/Quick_setup), return an appropriate set of temporary credentials.
-     * 
-     * The supplied audience must be on a whitelist of TaskCluster-related
-     * sites configured in the login service.  This is not a general-purpose
-     * assertion-verification service!
-     *
-     * @see "[Get TaskCluster credentials given a Persona assertion API Documentation](https://docs.taskcluster.net/reference/core/login/api-docs#credentialsFromPersonaAssertion)"
-     */
-    public CallSummary<PersonaAssertionRequest, CredentialsResponse> credentialsFromPersonaAssertion(PersonaAssertionRequest payload) throws APICallFailure {
-        return apiCall(payload, "POST", "/persona", CredentialsResponse.class);
     }
 
     /**
