9caeb4a2eb44503637b925092a6bc483579d902d
Pete Moore <pmoore@mozilla.com>
2017-02-01 20:30:31 +0100

    Regenerated library to pick up latest schema changes

--- src/main/java/org/mozilla/taskcluster/client/auth/CreateClientRequest.java
+++ src/main/java/org/mozilla/taskcluster/client/auth/CreateClientRequest.java
@@ -10,6 +10,17 @@ import java.util.Date;
 public class CreateClientRequest {
 
     /**
+     * If `true`, the service may delete this client after it has expired.  If
+     * `false` (the default), the client will remain after expiration, although
+     * it cannot be used for authentication in that state.
+     *
+     * Default:    false
+     *
+     * See http://schemas.taskcluster.net/auth/v1/create-client-request.json#/properties/deleteOnExpiration
+     */
+    public boolean deleteOnExpiration;
+
+    /**
      * Description of what these credentials are used for in markdown.
      * Should include who is the owner, point of contact.
      *
--- src/main/java/org/mozilla/taskcluster/client/auth/GetClientResponse.java
+++ src/main/java/org/mozilla/taskcluster/client/auth/GetClientResponse.java
@@ -26,6 +26,15 @@ public class GetClientResponse {
     public Date created;
 
     /**
+     * If `true`, the service may delete this client after it has expired.  If
+     * `false`, the client will remain after expiration, although it cannot be
+     * used for authentication in that state.
+     *
+     * See http://schemas.taskcluster.net/auth/v1/get-client-response.json#/properties/deleteOnExpiration
+     */
+    public boolean deleteOnExpiration;
+
+    /**
      * Description of what these credentials are used for in markdown.
      * Should include who is the owner, point of contact.
      *
