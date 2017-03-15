266ef74ec7d7cf536bd0e8393a6afa516c73f6df
Pete Moore <pmoore@mozilla.com>
2017-03-15 19:29:12 +0100

    Regenerated library to pick up latest schema changes

--- src/main/java/org/mozilla/taskcluster/client/notify/SendEmailRequest.java
+++ src/main/java/org/mozilla/taskcluster/client/notify/SendEmailRequest.java
@@ -72,4 +72,16 @@ public class SendEmailRequest {
      * See http://schemas.taskcluster.net/notify/v1/email-request.json#/properties/subject
      */
     public String subject;
+
+    /**
+     * E-mail html template used to format your content.
+     *
+     * Possible values:
+     *     * "simple"
+     *     * "fullscreen"
+     * Default:    "simple"
+     *
+     * See http://schemas.taskcluster.net/notify/v1/email-request.json#/properties/template
+     */
+    public String template;
 }
