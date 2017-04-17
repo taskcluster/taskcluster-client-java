71bf72a9191f844bab312349d045e7c3199cd000
Pete Moore <pmoore@mozilla.com>
2017-04-17 22:33:26 +0200

    Regenerated library to pick up latest schema changes

--- src/main/java/org/mozilla/taskcluster/client/pulse/NamespaceCreationResponse.java
+++ src/main/java/org/mozilla/taskcluster/client/pulse/NamespaceCreationResponse.java
@@ -10,6 +10,14 @@ import java.util.Date;
 public class NamespaceCreationResponse {
 
     /**
+     * The AMQP URL for connecting to the pulse service.  Note that this URL
+     * contains a password, so it should not be logged or displayed to users.
+     *
+     * See http://schemas.taskcluster.net/pulse/v1/namespace-response.json#/properties/connectionString
+     */
+    public String connectionString;
+
+    /**
      * E-mail address that will reach people who can address problems with runaway queue length.
      * The service will send warning notifications to this address before forcibly deleting the
      * queue.
@@ -41,24 +49,10 @@ public class NamespaceCreationResponse {
     public String namespace;
 
     /**
-     * The password created for authentication
-     *
-     * See http://schemas.taskcluster.net/pulse/v1/namespace-response.json#/properties/password
-     */
-    public String password;
-
-    /**
      * The caller should plan to call `claimNamespace` again at this time. The provided
      * password will become invalid a short time after this.
      *
      * See http://schemas.taskcluster.net/pulse/v1/namespace-response.json#/properties/reclaimAt
      */
     public Date reclaimAt;
-
-    /**
-     * The username created for authentication
-     *
-     * See http://schemas.taskcluster.net/pulse/v1/namespace-response.json#/properties/username
-     */
-    public String username;
 }
